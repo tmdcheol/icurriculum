618159419edcdbfb99646f54d27e777554d5d15e
tmdcheol <leesc08392@gmail.com>
2024-08-07 20:01:38 +0900

    feat: MemberMajor 조회테스트 추가
    - spring data jpa 의존 테스트
    - 테스트용 데이터와 실제 조회 데이터가 일치하는지 확인
    - fetch join 사용

--- src/main/java/icurriculum/domain/membermajor/repository/MemberMajorRepository.java
+++ src/main/java/icurriculum/domain/membermajor/repository/MemberMajorRepository.java
@@ -3,9 +3,14 @@ package icurriculum.domain.membermajor.repository;
 import icurriculum.domain.member.Member;
 import icurriculum.domain.membermajor.MemberMajor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberMajorRepository extends JpaRepository<MemberMajor, Long> {
-    List<MemberMajor> findByMember(Member Member);
+
+    @Query("select mm from MemberMajor mm join fetch mm.member m join fetch mm.department d where mm.member = :member")
+    List<MemberMajor> findByMember(@Param("member") Member member);
+
 }
--- src/main/java/icurriculum/domain/take/repository/TakeRepository.java
+++ src/main/java/icurriculum/domain/take/repository/TakeRepository.java
@@ -5,12 +5,13 @@ import icurriculum.domain.member.Member;
 import icurriculum.domain.take.Take;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TakeRepository extends JpaRepository<Take, Long> {
 
     @Query("select t from Take t join fetch t.member m left join fetch t.course c where t.member = :member")
-    List<Take> findByMember(Member member);
+    List<Take> findByMember(@Param("member") Member member);
 
 }
